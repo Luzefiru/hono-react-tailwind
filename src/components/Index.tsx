a54f042e17bf9c15e2ec28683ce88faeab5eb2b1
Luzefiru <ian.deejay.iv@gmail.com>
2024-05-23 21:43:34 +0800

    fix: remove PicoCSS and improve boilerplate
    Show that routing works with React Router.

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="p-8 flex flex-col items-center justify-center h-screen">
-      <h1 className="text-yellow-500 text-4xl">Hello World</h1>
+    <div className="flex flex-col items-center justify-center h-screen gap-2 p-8">
+      <h1 className="text-4xl text-yellow-500">Hono + React + Tailwind</h1>
       <p>Current Count: {count}</p>
       <div>
-        <button onClick={() => setCount((prev) => prev + 1)}>Click Me!</button>
+        <button
+          className="px-4 py-2 font-bold bg-red-500 rounded-lg hover:bg-red-700"
+          onClick={() => setCount((prev) => prev + 1)}
+        >
+          Click Me!
+        </button>
       </div>
+      <a className="text-blue-500 hover:text-blue-700" href="/hello/John">
+        Click to navigate to <code>/hello/John</code>
+      </a>
     </div>
   );
 }
